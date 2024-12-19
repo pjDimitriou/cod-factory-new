34cffab2f98f728eec66f62972bc517dd848ab5e
2-Deep <dimitrioupeterjohn@gmail.com>
2024-12-19 16:44:17 +0200

    initial commit

---
+++ src/gr/aueb/cf/ch1/HelloWorld.java
@@ -0,0 +1,16 @@
+package gr.aueb.cf.ch1;
+
+
+/**
+ * Prints the phrase 'Hello World'
+ * in the standard output (console)
+ *
+ * @author a8ana
+ */
+
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
